e0384de2693e8fe82e249b2fcd570dc458ee728f
mario-github <46192072+mario-github@users.noreply.github.com>
2018-12-27 23:00:42 +1100

    Added the next two lines of poem

--- main.rs
+++ main.rs
@@ -1,5 +1,8 @@
 fn main() {
-    println!("Jack and Jill went up a hill");
+	println!("Jack and Jill went up a hill");
 	println!("to fetch a pail of water;");
-	println!("HELP PLEASE! Need Assistance with next two lines");
+	/*HELP PLEASE! Need Assistance with next two lines */
+	//Please see the remaining two lines you were after!
+	println!("Jack fell down, and broke his crown,");
+	println!("and Jill came tumbling after.");
 }
